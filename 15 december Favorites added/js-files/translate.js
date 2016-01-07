3f4cfdb5823c8bcf2352d9ad000fd73814179fd9
MalinEd <malin.edlert@gmail.com>
2016-01-07 13:13:06 +0100

    Update translate.js
    now this file should work better

--- 15 december Favorites added/js-files/translate.js
+++ 15 december Favorites added/js-files/translate.js
@@ -22,6 +22,7 @@ function Translate(language) {
     }
     }
 
+
 function English() {
     return [
         ["sitetitle", "Welcome Aboard The Flying Dutchman!"],
@@ -31,38 +32,55 @@ function English() {
         ["credit", "<h5>Your credit: ...</h5>"],
         ["cost", "<h5>Total cost: ....</h5>"],
         ["opener", "Pay"],
-        ["yourorder", "Your order"],
+        ["yourorder", "Your order:"],
+        ["favs", "<b id='b1' class ='current'>Favorites</b>"],
+        ["popular", "<b id='b2'>Popular</b>"],
+        ["all1", "<c id='c1' class='current'>Beverages</c>"],
+        ["öl1", "<c id='c2' >Beer</c>"],
+        ["cider1", "<c id='c3' >Cider</c>"],
+        ["vitt_vin1", "<c id='c4' >White wine</c>"],
+        ["rött_vin1","<c id='c5' >Red wine</c>"],
+        ["alcohol-free1","<c id='c6' >Alcohol free</c>"],
+        ["searching1", "<c id='c7' >Search</c>"],
+        ["search_2","Search beverage"],
+        ["inf1","ⓘ=Order from bartender"],
+        ["inf2","ⓘ=Order from bartender"],
+        ["favMessage","You have not added any favorites"],
         ["wannaPay", "<p>Are you sure you would like to pay?</p>"],
-        ["beverages", "Beverages"],
-        ["favs", "Favorites"],
-        ["popular", "Popular"],
-        ["favBeverage", "Favorites"],
-        ["popularBev", "<h3>Most sold Beverages</h3>"],
-        ["drinks", "Beverages"],
-        ["searching1", "Search beverage"]
+
     ]
+
 }
 
 function Svenska() {
-    return [
-        ["sitetitle", "Välkommne ombord på The Flying Dutchman!"],
-        ["logoutbutton", "Logga ut"],
-        ["leftbar1", "Min sida"],
-        ["leftbar2", "Drycker"],
-        ["credit", "<h5>Ditt kredit: ...</h5>"],
-        ["cost", "<h5>Total kostnad: ....</h5>"],
-        ["yourorder", "Din beställning"],
-        ["opener", "Betala"],
-        ["wannaPay", "<p>Är du säker att du vill betala?</p>"],
-        ["beverages", "Drycker"],
-        ["favs", "Favoriter"],
-        ["popular", "Populära"],
-        ["favBeverage", "Favoriter"],
-        ["popularBev", "<h3>Mest sålda drycker</h3>"],
-        ["drinks", "Drycker"],
-        ["searching1", "Sök dryck"]
+    return [ 
+        ["sitetitle", "Välkommne ombord på The Flying Dutchman!"], 
+        ["logoutbutton", "Logga ut"], 
+        ["leftbar1", "Min sida"], 
+        ["leftbar2", "Drycker"], 
+        ["credit", "<h5>Din kredit: ...</h5>"], 
+        ["cost", "<h5>Total kostnad: ....</h5>"], 
+        ["yourorder", "Din beställning:"], 
+        ["opener", "Betala"], 
+        ["favs", "<b id='b1' class ='current'>  Favoriter  </b>"], 
+        ["popular","<b id='b2' >Populära</b>"], 
+        ["all1", "<c id='c1' class='current'>Drycker</c>"], 
+        ["öl1", "<c id='c2'>Öl</c>"], 
+        ["cider1", "<c id='c3' >Cider</c>"], 
+        ["vitt_vin1", "<c id='c4' >Vitt vin</c>"], 
+        ["rött_vin1","<c id='c5' >Rött vin</c>"], 
+        ["alcohol-free1","<c id='c6' >Alcohol fri</c>"], 
+        ["searching1", "<c id='c7' >Search</c>"], 
+        ["search_2","Sök dryck"],
+        ["inf1","ⓘ=Beställ av bartendern"],
+        ["inf2","ⓘ=Beställ av bartendern"],
+        ["favMessage","Du har inte lagt till några favoriter"],
+        ["wannaPay", "<p>Är du säker att du vill betala?</p>"]
+
     ]
+
 }
+
 
 function Español() {
     return [
@@ -72,15 +90,23 @@ function Español() {
         ["leftbar2", "Alcoholes"],
         ["credit", "<h5>Su crédito: ...</h5>"],
         ["cost", "<h5>Costo total: ....</h5>"],
-        ["yourorder", "Your order"],
+        ["yourorder", "Su pedido:"],
         ["opener", "Pagar"],
-        ["wannaPay", "<p> ¿Está seguro que desea pagar? </ p>"],
-        ["beverages", "Alcoholes"],
-        ["favs", "Favoritos"],
-        ["popular", "Popular"],
-        ["favBeverage", "Favoritos"],
-        ["popularBev", "<h3>Bedidas más vendidos</h3>"],
-        ["drinks", "Bedidas"],
-        ["searching1", "Búsqueda de bebidas"]]
+        ["favs", "<b id='b1' class='current'>Favoritos</b>"],
+        ["popular", "<b id='b2' >Popular</b>"],
+        ["all1", "<c  id='c1' class='current'>Bedidas</c>"],
+        ["öl1", "<c id='c2' >Cerveza</c>"],
+        ["cider1", "<c id='c3' >Sidra</c>"],
+        ["vitt_vin1", "<c id='c4' >Vino blanco</c>"],
+        ["rött_vin1","<c id='c5' >Vino tinto</c>"],
+        ["alcohol-free1","<c id='c6' >No alcohólico</c>"],
+        ["searching1", "<c id='c7' >Buscar</c>"],
+        ["search_2","Buscar"],
+        ["inf1","ⓘ=Fin de camarero"],
+        ["inf2","ⓘ=Fin de camarero"],
+        ["favMessage","Usted no ha agregado favoritos"],
+        ["wannaPay", "<p> ¿Está seguro que desea pagar? </p>"],
+
+    ]
 }
 
